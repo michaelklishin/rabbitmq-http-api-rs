7e10c40283b604150e24f336609eadf58707b268
Michael Klishin <michaelklishin@icloud.com>
2025-01-19 23:16:59 -0500

    Port two more test suites

---
+++ tests/async_node_memory_footprint_tests.rs
@@ -0,0 +1,44 @@
+// Copyright (C) 2023-2025 RabbitMQ Core Team (teamrabbitmq@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use rabbitmq_http_client::api::Client;
+
+mod test_helpers;
+use crate::test_helpers::{endpoint, PASSWORD, USERNAME};
+
+use regex::Regex;
+
+#[tokio::test]
+async fn test_async_get_node_memory_footprint() {
+    let endpoint = endpoint();
+    let rc = Client::new(&endpoint, USERNAME, PASSWORD);
+    let nodes = rc.list_nodes().await.unwrap();
+    let name = nodes.first().unwrap().name.clone();
+    let footprint = &mut rc.get_node_memory_footprint(&name).await.unwrap();
+
+    assert!(footprint.breakdown.total.rss >= 1);
+    assert!(footprint.breakdown.total.allocated >= 1);
+    assert!(footprint.breakdown.total.used_by_runtime >= 1);
+    assert!(footprint.breakdown.grand_total() >= 1);
+
+    assert!(footprint.breakdown.metadata_store >= 1);
+    assert!(footprint.breakdown.code >= 1);
+
+    let regex = Regex::new(r"\d+\.\d+%").unwrap();
+
+    let metadata_store_percentage_s = footprint.breakdown.metadata_store_percentage_as_text();
+    assert!(regex.is_match(&metadata_store_percentage_s));
+
+    let code_percentage_s = footprint.breakdown.code_percentage_as_text();
+    assert!(regex.is_match(&code_percentage_s));
+}
---
+++ tests/async_permission_tests.rs
@@ -0,0 +1,173 @@
+// Copyright (C) 2023-2025 RabbitMQ Core Team (teamrabbitmq@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use rabbitmq_http_client::requests::VirtualHostParams;
+use rabbitmq_http_client::responses;
+use rabbitmq_http_client::{api::Client, requests::Permissions};
+
+mod test_helpers;
+use crate::test_helpers::{endpoint, PASSWORD, USERNAME};
+
+#[tokio::test]
+async fn test_async_list_permissions() {
+    let endpoint = endpoint();
+    let rc = Client::new(&endpoint, USERNAME, PASSWORD);
+
+    let vh_params = VirtualHostParams::named("test_list_permissions");
+    let _ = rc.delete_vhost(vh_params.name, false);
+    let result1 = rc.create_vhost(&vh_params).await;
+    assert!(result1.is_ok());
+
+    let result = rc.list_permissions().await;
+    assert!(result.is_ok());
+
+    let vec = result.unwrap();
+    assert!(vec.iter().any(|p| p
+        == &responses::Permissions {
+            user: "guest".to_owned(),
+            vhost: "test_list_permissions".to_owned(),
+            configure: ".*".to_owned(),
+            read: ".*".to_owned(),
+            write: ".*".to_owned(),
+        }));
+
+    rc.delete_vhost(vh_params.name, false).await.unwrap();
+}
+
+#[tokio::test]
+async fn test_async_list_permissions_in() {
+    let endpoint = endpoint();
+    let rc = Client::new(&endpoint, USERNAME, PASSWORD);
+
+    let vh_params = VirtualHostParams::named("test_list_permissions_in");
+    let _ = rc.delete_vhost(vh_params.name, false);
+    let result1 = rc.create_vhost(&vh_params).await;
+    assert!(result1.is_ok());
+
+    let result = rc.list_permissions_in("test_list_permissions_in").await;
+    assert!(result.is_ok(), "list_permissions_in returned {:?}", result);
+
+    let vec = result.unwrap();
+    assert!(vec.iter().any(|p| p
+        == &responses::Permissions {
+            user: "guest".to_owned(),
+            vhost: "test_list_permissions_in".to_owned(),
+            configure: ".*".to_owned(),
+            read: ".*".to_owned(),
+            write: ".*".to_owned(),
+        }));
+
+    rc.delete_vhost(vh_params.name, false).await.unwrap();
+}
+
+#[tokio::test]
+async fn test_async_list_permissions_of() {
+    let endpoint = endpoint();
+    let rc = Client::new(&endpoint, USERNAME, PASSWORD);
+
+    let vh_params = VirtualHostParams::named("test_list_permissions_of");
+    let _ = rc.delete_vhost(vh_params.name, false).await;
+    let result1 = rc.create_vhost(&vh_params).await;
+    assert!(result1.is_ok());
+
+    let result = rc.list_permissions_of("guest").await;
+    assert!(result.is_ok(), "list_permissions_of returned {:?}", result);
+
+    let vec = result.unwrap();
+    assert!(vec.iter().any(|p| p
+        == &responses::Permissions {
+            user: "guest".to_owned(),
+            vhost: "test_list_permissions_of".to_owned(),
+            configure: ".*".to_owned(),
+            read: ".*".to_owned(),
+            write: ".*".to_owned(),
+        }));
+
+    rc.delete_vhost(vh_params.name, false).await.unwrap();
+}
+
+#[tokio::test]
+async fn test_async_get_permissions() {
+    let endpoint = endpoint();
+    let rc = Client::new(&endpoint, USERNAME, PASSWORD);
+
+    let vh_params = VirtualHostParams::named("test_get_permissions");
+    let _ = rc.delete_vhost(vh_params.name, false).await;
+    let result1 = rc.create_vhost(&vh_params).await;
+    assert!(result1.is_ok());
+
+    let result2 = rc.get_permissions("test_get_permissions", "guest").await;
+    assert!(
+        result2.is_ok(),
+        "list_permissions_of returned {:?}",
+        result2
+    );
+
+    let permissions = result2.unwrap();
+    assert_eq!(
+        permissions,
+        responses::Permissions {
+            user: "guest".to_owned(),
+            vhost: "test_get_permissions".to_owned(),
+            configure: ".*".to_owned(),
+            read: ".*".to_owned(),
+            write: ".*".to_owned(),
+        }
+    );
+
+    rc.delete_vhost(vh_params.name, false).await.unwrap();
+}
+
+#[tokio::test]
+async fn test_async_grant_permissions() {
+    let endpoint = endpoint();
+    let rc = Client::new(&endpoint, USERNAME, PASSWORD);
+
+    let vh_params = VirtualHostParams::named("test_grant_permissions");
+    let _ = rc.delete_vhost(vh_params.name, false).await;
+    let result1 = rc.create_vhost(&vh_params).await;
+    assert!(result1.is_ok());
+
+    let params = Permissions {
+        user: "guest",
+        vhost: vh_params.name,
+        configure: "configure",
+        read: "read",
+        write: "write",
+    };
+    let result = rc.declare_permissions(&params).await;
+    assert!(result.is_ok(), "declare_permissions returned {:?}", result);
+
+    let result2 = rc.get_permissions(vh_params.name, "guest").await;
+    assert!(result2.is_ok(), "get_permissions_of returned {:?}", result2);
+
+    let result3 = result2.unwrap();
+    assert_eq!(
+        result3,
+        responses::Permissions {
+            user: "guest".to_owned(),
+            vhost: vh_params.name.to_owned(),
+            configure: "configure".to_owned(),
+            read: "read".to_owned(),
+            write: "write".to_owned(),
+        }
+    );
+
+    let result4 = rc.grant_permissions(vh_params.name, "guest").await;
+    assert!(result4.is_ok(), "delete_permissions returned {:?}", result4);
+
+    let result5 = rc.get_permissions(vh_params.name, "guest").await;
+    assert!(result5.is_err(), "permissions found after deletion");
+
+    rc.delete_vhost(vh_params.name, false).await.unwrap();
+}
